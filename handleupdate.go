5ddb9f71dba115491c771984bf593d5bf9323465
Gianguido Sorà <g.sora4@gmail.com>
2017-11-24 16:48:25 +0100

    added test print for callback queries

--- handleupdate.go
+++ handleupdate.go
@@ -68,6 +68,8 @@ func handleUpdate(update tgbotapi.Update) {
 			debugPrint("called handleDefault()")
 			handleDefault(int64(update.Message.From.ID))
 		}
+	} else if update.CallbackQuery != nil {
+		log.Println("got callback query with message:", update.CallbackQuery.Message.Text)
 	}
 }
 
