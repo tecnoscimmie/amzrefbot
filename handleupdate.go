99dc859760278d35563910c7b2677bdaf0af53d7
Gianguido Sorà <g.sora4@gmail.com>
2017-11-24 16:59:11 +0100

    move the core of message handling in a function this way, we can re-use that code in the inline keyboard

--- handleupdate.go
+++ handleupdate.go
@@ -34,42 +34,47 @@ func handleUpdate(update tgbotapi.Update) {
 	} else if update.Message != nil {
 		debugPrint("received a private message from " + update.Message.From.UserName)
 		// private message, handle various messages
-		message := strings.Split(update.Message.Text, " ")
-		debugPrint(fmt.Sprintln("parsed message array:", message))
-		debugPrint("command -> " + message[0])
-		switch message[0] { // the first element of the array is usually the command
-		case ListPendingCommand:
-			debugPrint("called handleListPending()")
-			handleListPending(int64(update.Message.From.ID))
-		case DenyCommand:
-			adminErr := checkIfAdministrator(update.Message.From.ID)
-			paramsErr := checkIfHasParameter(message, update.Message.From.ID, "deny")
-			if adminErr != nil || paramsErr != nil {
-				return
-			}
-			debugPrint("called handleDeny()")
-			handleDeny(message[1])
-		case AllowCommand:
-			adminErr := checkIfAdministrator(update.Message.From.ID)
-			paramsErr := checkIfHasParameter(message, update.Message.From.ID, "allow")
-			if adminErr != nil || paramsErr != nil {
-				return
-			}
-			debugPrint("called handleAllow()")
-			handleAllow(message[1])
-		case AddCodeCommand:
-			paramsErr := checkIfHasParameter(message, update.Message.From.ID, "addcode")
-			if paramsErr != nil {
-				return
-			}
-			debugPrint("called handleAddCode()")
-			handleAddCode(update.Message.From.UserName, message[1], int64(update.Message.From.ID))
-		default:
-			debugPrint("called handleDefault()")
-			handleDefault(int64(update.Message.From.ID))
-		}
+		handleTelegramMessage(update.Message.Text, int64(update.Message.From.ID), update.Message.From.UserName)
 	} else if update.CallbackQuery != nil {
-		log.Println("got callback query with message:", update.CallbackQuery.Data)
+		log.Println("got callback query with message:")
+		handleTelegramMessage(update.CallbackQuery.Data, int64(update.CallbackQuery.From.ID), update.CallbackQuery.From.UserName)
+	}
+}
+
+func handleTelegramMessage(messageBlob string, chatid int64, username string) {
+	message := strings.Split(messageBlob, " ")
+	debugPrint(fmt.Sprintln("parsed message array:", message))
+	debugPrint("command -> " + message[0])
+	switch message[0] { // the first element of the array is usually the command
+	case ListPendingCommand:
+		debugPrint("called handleListPending()")
+		handleListPending(chatid)
+	case DenyCommand:
+		adminErr := checkIfAdministrator(int(chatid))
+		paramsErr := checkIfHasParameter(message, int(chatid), "deny")
+		if adminErr != nil || paramsErr != nil {
+			return
+		}
+		debugPrint("called handleDeny()")
+		handleDeny(message[1])
+	case AllowCommand:
+		adminErr := checkIfAdministrator(int(chatid))
+		paramsErr := checkIfHasParameter(message, int(chatid), "allow")
+		if adminErr != nil || paramsErr != nil {
+			return
+		}
+		debugPrint("called handleAllow()")
+		handleAllow(message[1])
+	case AddCodeCommand:
+		paramsErr := checkIfHasParameter(message, int(chatid), "addcode")
+		if paramsErr != nil {
+			return
+		}
+		debugPrint("called handleAddCode()")
+		handleAddCode(username, message[1], chatid)
+	default:
+		debugPrint("called handleDefault()")
+		handleDefault(chatid)
 	}
 }
 
